720545b67ff93c0925ba0723fda578d470d24a72
B1don228 <fergyr2121@gmail.com>
2024-02-09 14:07:35 +0200

    GrapghQL 1.0

---
+++ src/components/Graph.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  HttpLink,
+  from,
+} from "@apollo/client";
+
+import { onError } from "@apollo/client/link/error";
+
+const errorLink = onError(({ grapghqlErrors, networkError }: any) => {
+  if (grapghqlErrors) {
+    grapghqlErrors.map(({ message, location, path }: any) => {
+      alert(`GraphQL error ${message}`);
+    });
+  }
+});
+
+const link = from([
+  errorLink,
+  new HttpLink({
+    uri: "https://swapi-graphql.netlify.app/.netlify/functions/index",
+  }),
+]);
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: link,
+});
+
+const Graph = () => {
+  return <ApolloProvider client={client}></ApolloProvider>;
+};
+
+export default Graph;
